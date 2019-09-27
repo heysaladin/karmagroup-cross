a4a78ca47a09f9b3c8a118710255a356cc753df7
heysaladin <heysaladin@gmail.com>
2019-09-27 18:04:13 +0800

    init

---
+++ android/app/src/main/kotlin/com/heysaladin/karmagroup_cross/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.heysaladin.karmagroup_cross
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+import 'package:karmagroup_cross/src/app.dart';
+
+void main() => runApp(App());
